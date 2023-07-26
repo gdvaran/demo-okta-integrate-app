60883b8be17d150efad5eb610f7932c6571cde34
gdvaran <62684179+gdvaran@users.noreply.github.com>
2023-07-27 00:41:36 +0530

    draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { ContainerComponent } from './container/container.component';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://trial-5168903.okta.com/oauth2/default',
+  clientId: '0oa6l1xtcaGFiuhYY697',
+  redirectUri: 'https://stackblitz-starters-kktazf.stackblitz.io/login/callback',
+});
+
+const oktaConfig = {
+  oktaAuth: oktaAuth,
+};
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, OktaAuthModule.forRoot(oktaConfig)],
   bootstrap: [ContainerComponent],
   declarations: [ContainerComponent],
 })
--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.css']
+  styleUrls: ['./container.component.css'],
 })
 export class ContainerComponent implements OnInit {
+  constructor(
+    private _router: Router,
+    private _oktaStateService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public async signIn() {
+    console.log('signIn clicked');
+    await this._oktaAuth.signInWithRedirect();
   }
 
+  public async signOut(): Promise<void> {
+    console.log('signOut clicked');
+    await this._oktaAuth.signOut();
+  }
 }
